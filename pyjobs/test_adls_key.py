06906b92f9ebad925dd1395438b6f0ca5b556faa
Gleb Smolnik <hsmol@softserveinc.com>
2020-10-28 13:22:33 +0100

    Added node assignment

--- pyjobs/test_adls_key.py
+++ pyjobs/test_adls_key.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     
     print("SPARK DIST CLASSPATH IS: ", os.getenv('SPARK_DIST_CLASSPATH'))
     print("HADOOP HOME IS: ", os.getenv('HADOOP_HOME'))
-    spark.conf.set("fs.azure.account.key.sparkstor.dfs.core.windows.net", "l9QpO1pw2Qsb+7Cxi+I/j5rflDDqB6aJs2yXDIdh8k6m7XVD+UJnqQ/6+A9BQyt8pDNeS8jwmVdK2SyYMP7PTw==")
+    spark.conf.set("fs.azure.account.key.sparkstor.dfs.core.windows.net", "$key$")
     n = spark.read.csv("abfss://sparkdata@sparkstor.dfs.core.windows.net/files", header=True).count()
     print("Lines number is %s" % n)
 
