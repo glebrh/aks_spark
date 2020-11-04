30315503bfc50fae865f176cf87cd4dfba54181f
Gleb Smolnik <hsmol@softserveinc.com>
2020-11-04 11:27:58 +0100

    SMTH

---
+++ Demo 2/test_adls_wout_creds.py
@@ -0,0 +1,32 @@
+from __future__ import print_function
+
+import sys
+from random import random
+from operator import add
+import os
+
+from pyspark.sql import SparkSession
+
+
+if __name__ == "__main__":
+    """
+        Usage: pi [partitions]
+    """
+    spark = SparkSession\
+        .builder\
+        .appName("ADLSTest")\
+        .getOrCreate()
+    
+    CLIENT_ID=os.getenv("CLIENT_ID")
+    CLIENT_SECRET=os.getenv("CLIENT_SECRET")
+    spark.conf.set("fs.azure.account.auth.type.sparkstor.dfs.core.windows.net", "OAuth")
+    spark.conf.set("fs.azure.account.oauth.provider.type.sparkstor.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider")
+    spark.conf.set("fs.azure.account.oauth2.client.id.sparkstor.dfs.core.windows.net", CLIENT_ID)
+    spark.conf.set("fs.azure.account.oauth2.client.secret.sparkstor.dfs.core.windows.net", CLIENT_SECRET)
+    spark.conf.set("fs.azure.account.oauth2.client.endpoint.sparkstor.dfs.core.windows.net", "https://login.microsoftonline.com/6c51c659-9d52-41af-81f7-dde16380e813/oauth2/token")
+    
+    
+    n = spark.read.csv("abfss://sparkdata@sparkstor.dfs.core.windows.net/files", header=True).count()
+    print("Lines number is %s" % n)
+
+    spark.stop()
